ad389a1e80936197ec410b235f07029f9560923d
Giovanni Bondioli <giovanni@Michelles-MacBook-Pro.local>
2018-03-03 18:29:09 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "King", "King"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+	];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -13,9 +35,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log( "User flipped " + cards[cardId] );
-	cardsInPlay.push(cards[cardId]);
-
+	console.log( "User flipped " + cards[cardId].rank );
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if ( cardsInPlay.length === 2 ) {
 		checkForMatch();
 	}
@@ -23,4 +46,5 @@ var flipCard = function(cardId) {
 
 flipCard(0);
 flipCard(2);
+
 
